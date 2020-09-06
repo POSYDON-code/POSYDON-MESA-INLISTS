065f3b41786fb2a726d6ebd49f5ea9773229e980
yingqin2019 <ying.qin@northwestern.edu>
2020-09-06 12:00:17 -0500

    added Eddington accretion limit for all COs

--- r11701/NS-He_star/binary/src/run_binary_extras.f
+++ r11701/NS-He_star/binary/src/run_binary_extras.f
@@ -401,6 +401,40 @@
 
       end function k_div_T
 
+      !! Eddington accreton limits for WD, NS and BH
+      subroutine my_mdot_edd(binary_id, mdot_edd, ierr)
+         use const_def, only: dp
+         integer, intent(in) :: binary_id
+         real(dp), intent(out) :: mdot_edd
+         integer, intent(out) :: ierr
+         real(dp) :: acc_radius, mdot_edd_eta
+         type (binary_info), pointer :: b
+         ierr = 0
+         call binary_ptr(binary_id, b, ierr)
+         if (ierr /= 0) then
+            write(*,*) 'failed in binary_ptr'
+            return
+         end if
+         if (b% m(2)/Msun <= 2.50) then ! WD and NS
+             if (0.17 <= b% m(2)/Msun .and. b% m(2)/Msun <= 1.25) then !Radius for WD
+                   acc_radius = 10 ** (-2) * ( (b% m(2)/Msun) ** (-1/3)) * Rsun !in cm
+             else if (1.25 < b% m(2)/Msun .and. b% m(2)/Msun <= 2.50) then !Radius for NS
+                   acc_radius = 11.0 * 10 ** 5 !in cm
+             end if
+             !! mdot_edd_eta for WD and NS
+             mdot_edd_eta = b% s_donor% cgrav(1) * b% m(2) / (clight ** 2 * acc_radius)
+         else! M2 > 2.5 Msol for BHs
+             !! mdot_edd_eta for BH
+             mdot_edd_eta = 1d0 &
+                      - sqrt(1d0 - (min(b% m(b% a_i),sqrt(6d0)*b% eq_initial_bh_mass)/(3d0*b% eq_initial_bh_mass))**2)
+              
+          mdot_edd = 4d0*pi*b% s_donor% cgrav(1)*b% m(b% a_i)&
+              /(clight*0.2d0*(1d0+b% s_donor% surface_h1)* mdot_edd_eta)    
+
+          !b% s1% x_ctrl(1) used to adjust the Eddington limit in inlist1
+          mdot_edd = mdot_edd * b% s1% x_ctrl(1)
+      end subroutine my_mdot_edd
+
       integer function how_many_extra_binary_history_columns(binary_id)
          use binary_def, only: binary_info
          integer, intent(in) :: binary_id
