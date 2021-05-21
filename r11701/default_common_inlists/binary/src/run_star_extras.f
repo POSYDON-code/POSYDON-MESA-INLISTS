692bee7c62dc97751ff6f4b6c7bc27deeee04bd9
ezapartas <ezapartas@gmail.com>
2021-05-21 18:48:38 +0200

    better syntax and turned off the LBV winds by default

--- r11701/default_common_inlists/binary/src/run_star_extras.f
+++ r11701/default_common_inlists/binary/src/run_star_extras.f
@@ -1592,8 +1592,8 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
 
       if(s% x_logical_ctrl(3)) then ! Belczynski+2010 LBV2 winds (eq. 8) with factor 1
         if ((s% center_h1 < 1.0d-4) ) then  ! postMS
-            if ((s% L(1)/Lsun > 6d0*pow_cr(10d0,5)) .and. (pow_cr(10.0d0,-5) * s% r(1)/Rsun * pow_cr((s% L(1)/Lsun),0.5) > 1.0d0)) then ! Humphreys-Davidson limit
-              wind  = pow_cr(10.0d0,-4)
+            if ((s% L(1)/Lsun > 6d0 * 1.0d5) .and. (1.0d-5 * s% r(1)/Rsun * pow_cr((s% L(1)/Lsun),0.5) > 1.0d0)) then ! Humphreys-Davidson limit
+              wind  = 1.0d-4
               if (dbg) write(*,1) 'LBV Belczynski+2010 wind', wind
             endif
         endif
