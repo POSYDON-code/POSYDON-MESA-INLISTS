a7c0fa3a133813843a7872c8129cffa42a297b5a
ezapartas <ezapartas@gmail.com>
2022-12-15 16:00:01 +0200

    real(dp) :: current_wind_prscr changed to number

--- r11701/default_common_inlists/binary/src/run_star_extras.f
+++ r11701/default_common_inlists/binary/src/run_star_extras.f
@@ -254,7 +254,7 @@ contains
     logical :: sticking_to_energy_without_recombination_corr
     real(dp) :: XplusY_CO_core_mass_threshold
     logical :: have_30_value, have_10_value, have_1_value, have_co_value
-    character (len=strlen) :: current_wind_prscr
+    real(dp) :: current_wind_prscr
 
     ierr = 0
     call star_ptr(id, s, ierr)
@@ -1414,7 +1414,7 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
     character (len=strlen) :: scheme
     logical :: using_wind_scheme_mdot
     real(dp), parameter :: Zsolar = 0.0142d0 ! for Vink et al formula
-    character (len=strlen) :: current_wind_prscr
+    real(dp) :: current_wind_prscr
 
     logical, parameter :: dbg = .false.
 
@@ -1457,7 +1457,6 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
     !massive stars
     if(s% initial_mass >= 10._dp)then
        scheme = s% hot_wind_scheme
-       current_wind_prscr = scheme
        call eval_wind_for_scheme(scheme,wind)
        if (dbg) write(*,*) 'using hot_wind_scheme: "' // trim(scheme) // '"'
     !low-mass stars
@@ -1467,14 +1466,12 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
           !RGB/TPAGB switch goes here
           if (s% have_done_TP) then
              scheme = s% cool_wind_AGB_scheme
-             current_wind_prscr = scheme
              if (dbg) &
                   write(*,1) 'using cool_wind_AGB_scheme: "' // trim(scheme) // '"', &
                   center_h1, center_he4, s% RGB_to_AGB_wind_switch
 
           else
              scheme= s% cool_wind_RGB_scheme
-             current_wind_prscr = scheme
              if (dbg) write(*,*) 'using cool_wind_RGB_scheme: "' // trim(scheme) // '"'
 
           endif
@@ -1484,7 +1481,6 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
        if(T1 >= s% cool_wind_full_on_T)then
           !evaluate hot wind
           scheme="Dutch"
-          current_wind_prscr = scheme
           call eval_wind_for_scheme(scheme, hot_wind)
           if (dbg) write(*,*) 'using hot_wind_scheme: "' // trim(scheme) // '"'
 
@@ -1513,7 +1509,9 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
       character(len=strlen) :: scheme
       real(dp), intent(out) :: wind
       include 'formats'
+      real(dp) :: current_wind_prscr
 
+      current_wind_prscr = 0
       wind = 4d-13*(L1*R1/M1)/(Lsun*Rsun/Msun) ! in Msun/year
       if (dbg) write(*,1) 'wind', wind
       if (wind <= 0.0d0 .or. is_bad_num(wind)) then
@@ -1547,8 +1545,10 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
          if(dbg) write(*,1) 'Dutch_wind', wind
       else if (scheme == 'Reimers') then
          wind = wind * s% Reimers_scaling_factor
+         current_wind_prscr = 4
          if(dbg) write(*,1) 'Reimers_wind', wind
       else if (scheme == 'Vink') then
+         current_wind_prscr = 1
          call eval_Vink_wind(wind)
          wind = wind * s% Vink_scaling_factor
          if (dbg) write(*,1) 'Vink_wind', wind
@@ -1558,9 +1558,11 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
          if (dbg) write(*,1) 'Grafener_wind', wind
       else if (scheme == 'Blocker') then
          call eval_blocker_wind(wind)
+         current_wind_prscr = 5
          if (dbg) write(*,1) 'Blocker_wind', wind
       else if (scheme == 'de Jager') then
          call eval_de_Jager_wind(wind)
+         current_wind_prscr = 3
          wind = s% de_Jager_scaling_factor * wind
          if (dbg) write(*,1) 'de_Jager_wind', wind
       else if (scheme == 'van Loon') then
@@ -1582,7 +1584,7 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
         if ((s% center_h1 < 1.0d-4) ) then  ! postMS
             if ((s% L(1)/Lsun > 6.0d5) .and. &
               (1.0d-5 * s% r(1)/Rsun * pow_cr((s% L(1)/Lsun),0.5d0) > 1.0d0)) then ! Humphreys-Davidson limit
-              current_wind_prscr = 'LBV_wind'
+              current_wind_prscr = 6
               wind  = 1.0d-4
               if (dbg) write(*,1) 'LBV Belczynski+2010 wind', wind
             endif
@@ -1686,14 +1688,14 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
     subroutine eval_highT_Dutch(w)
       real(dp), intent(out) :: w
       include 'formats'
-      character (len=strlen) :: current_wind_prscr
+      real(dp) :: current_wind_prscr
       if (surface_h1 < 0.4d0) then ! helium rich Wolf-Rayet star: Nugis & Lamers
          w = 1d-11 * pow_cr(L1/Lsun,1.29d0) * pow_cr(Y,1.7d0) * sqrt(Zsurf)
          if (dbg) write(*,1) 'Dutch_wind = Nugis & Lamers', log10_cr(wind)
-        current_wind_prscr = "Nugis_Lamers"
+        current_wind_prscr = 2
       else
          call eval_Vink_wind(w)
-         current_wind_prscr = "Vink"
+         current_wind_prscr = 1
       end if
 
     end subroutine eval_highT_Dutch
@@ -1702,9 +1704,10 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
     subroutine eval_lowT_Dutch(w)
       real(dp), intent(out) :: w
       include 'formats'
-      character (len=strlen) :: current_wind_prscr
+      real(dp) :: current_wind_prscr
       if (s% Dutch_wind_lowT_scheme == 'de Jager') then
          call eval_de_Jager_wind(w)
+         current_wind_prscr = 3
          if (dbg) write(*,1) 'Dutch_wind = de Jager', safe_log10_cr(wind), T1, T_low, T_high
       else if (s% Dutch_wind_lowT_scheme == 'van Loon') then
          call eval_van_Loon_wind(w)
@@ -1717,7 +1720,6 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
               trim(s% Dutch_wind_lowT_scheme)
          w = 0d0
       end if
-     current_wind_prscr = s% Dutch_wind_lowT_scheme
     end subroutine eval_lowT_Dutch
 
 
