ce634db6f958d5087aa8f8e12c9fb4afec83bb8d
ZepeiX <77278187+ZepeiX@users.noreply.github.com>
2024-04-05 16:29:22 -0500

    Update run_star_extras.f

--- r11701/default_common_inlists/binary/src/run_star_extras.f
+++ r11701/default_common_inlists/binary/src/run_star_extras.f
@@ -137,6 +137,7 @@ contains
       s% x_logical_ctrl(5) = .false.
       write(*,*) 'deactivate', s% w_div_w_crit_avg_surf, s% omega(1)
     end if
+    m=0
     if (s% x_logical_ctrl(5)) then
       do k = 1, s% nz - 1
         kap = s% opacity(k)
@@ -147,12 +148,9 @@ contains
         end if
         dm = s% dm(k)
         dtau = dm*kap/(4*pi*rmid*rmid)
-        if (tau + dtau <= s% surf_avg_tau_min) then
-          tau = tau + dtau
-          cycle
-        end if
+        m = m + dm
         s% extra_omegadot(k) = (0.5d0 * sqrt(s% cgrav(k)* s% m_grav(k) / pow3(s% r_equatorial(k)))-s% omega(k))/s% dt
-        if (tau >= s% surf_avg_tau) exit
+        if (m >= 0.1d0*Msun ) exit
       end do
       write(*,*) 'modified', s% w_div_w_crit_avg_surf, s% omega(1), tau, s% surf_avg_tau
     end if
