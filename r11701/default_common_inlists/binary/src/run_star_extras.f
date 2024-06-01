f7204ec868e8d0bf376f3e8b9962968fe0d1e458
Seth Shearer Gossage <ssg9761@quser31.quest.it.northwestern.edu>
2024-06-01 11:40:59 -0500

    initialize current_wind_prscr with values of -1d0

--- r11701/default_common_inlists/binary/src/run_star_extras.f
+++ r11701/default_common_inlists/binary/src/run_star_extras.f
@@ -38,7 +38,7 @@ module run_star_extras
   logical :: post_AGB_check = .false.
   logical :: pre_WD_check = .false.
   logical :: stripped_He_check = .false.
-  real(dp) :: current_wind_prscr(2) 
+  real(dp) :: current_wind_prscr(2) = -1d0
 
 contains
 
