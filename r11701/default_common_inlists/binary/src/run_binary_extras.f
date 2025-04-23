2d33d35ce5de13b9335371e17cdf52e598e9f6f5
Debatri Chattopadhyay <36978935+debatric@users.noreply.github.com>
2025-04-23 11:16:29 -0500

    Update run_binary_extras.f
    further fixes

--- r11701/default_common_inlists/binary/src/run_binary_extras.f
+++ r11701/default_common_inlists/binary/src/run_binary_extras.f
@@ -164,12 +164,12 @@
          integer, intent(in) :: binary_id
          integer, intent(out) :: ierr
          real(dp) :: osep, q, M, rA1, m1dot_rlo, m2dot_rlo, gamma_fast, gamma_iso, ang_mom_j
-         real(dp) :: jdot, xfer_frac_rlo, x
+         real(dp) :: jdot, xfer_frac_rlo
          real(dp) :: m1dot_wind, m2dot_wind, xfer_frac_wind
 
          real(dp) :: jdot_wind_donor, jdot_wind_accretor, jdot_RLOF_donor, jdot_RLOF_accretor
          real(dp) :: adot_rlo, jdot_ecc_RLOF_accretor, edot_RLOF, jdot_rlo
-	 real(dp) :: cos_epsilon_tau
+	 real(dp) :: cos_epsilon_tau, x
 
          type (binary_info), pointer :: b
          ierr = 0
@@ -216,7 +216,7 @@
          x = eval_rlobe(b% m(b% d_i), b% m(b% a_i), osep) / b% r(b% d_i)
 	 
          ! Eccenctric mass transfer contribution - Eqn 39a Hamers & Dosopoulou (2018)
-         adot_rlo = - 2 * osep * m1dot_rlo / b% m(b% d_i) * (1d0 - q) *& 
+         adot_rlo = - 2d0 * osep * m1dot_rlo / b% m(b% d_i) * (1d0 - q) *& 
 	            ( 1d0 - 3d0 * pow_cr(b% eccentricity, 2) * x * cos_epsilon_tau/( x - 1d0 ) )
          
          ! Translate to binary ang. mom.
@@ -293,6 +293,7 @@
          integer, intent(out) :: ierr
          real(dp) :: osep, q, rA1, m2dot_rlo, m2dot_wind, gamma_fast, gamma_iso, M, ang_mom_j
          real(dp) :: m1dot_rlo, xfer_frac_rlo, edot_rlo
+	 real(dp) :: cos_epsilon_tau, x
          type (binary_info), pointer :: b
          ierr = 0
          call binary_ptr(binary_id, b, ierr)
@@ -317,7 +318,7 @@
          x = eval_rlobe(b% m(b% d_i), b% m(b% a_i), osep) / b% r(b% d_i)
 
          ! Calculate edot contribution - Eqn 39b, Hamers & Dosopoulou (2019)	 
-         edot_rlo = - 2 * osep * m1dot_rlo / b% m(b% d_i) * (1d0 - q) *& 
+         edot_rlo = - 2d0 * osep * m1dot_rlo / b% m(b% d_i) * (1d0 - q) *& 
 	            (  (3d0/2d0) * b% eccentricity * cos_epsilon_tau * x/( 1d0 - x ) )
 	     
          b% extra_edot = edot_rlo
