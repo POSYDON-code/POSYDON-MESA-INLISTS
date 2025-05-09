64ed69a7552a976fa0b74d49242f3afb0549ff11
Kyle Rocha <48293898+ka-rocha@users.noreply.github.com>
2025-05-08 17:48:24 -0700

    Update xfer_frac_rlo in edot to match jdot_ml

--- r11701/default_common_inlists/binary/src/run_binary_extras.f
+++ r11701/default_common_inlists/binary/src/run_binary_extras.f
@@ -216,7 +216,7 @@
          if (abs(b% mtransfer_rate/(Msun/secyer)) .ge. 1.0d-15) then
 	     xfer_frac_rlo = (b% m(b% a_i) - b% m_old(b% a_i)) / abs(b% mtransfer_rate * b% time_step * secyer)
              ! negative means a net mass loss from the accretor -> if in active RLO, gamma shold be zero
-	     write(*,*) "MT_gamma: ",  xfer_frac_rlo, b% time_step, b% m_old(b% a_i) - b% m(b% a_i)
+	     !write(*,*) "MT_gamma: ",  xfer_frac_rlo, b% time_step, b% m_old(b% a_i) - b% m(b% a_i)
          end if
          xfer_frac_rlo = min( max(0.0_dp, xfer_frac_rlo), 1.0_dp) ! bounded in [0,1]
 	 
@@ -291,7 +291,12 @@
          
 	 ! Calculate mass transfer efficiency
          !xfer_frac_rlo = b% xfer_fraction
-	 xfer_frac_rlo = b% m_old(b% a_i) - b% m(b% a_i) / abs(b% mtransfer_rate * b% dt)
+	 xfer_frac_rlo = 1.0_dp
+         if (abs(b% mtransfer_rate/(Msun/secyer)) .ge. 1.0d-15) then
+	     xfer_frac_rlo = (b% m(b% a_i) - b% m_old(b% a_i)) / abs(b% mtransfer_rate * b% time_step * secyer)
+             ! negative means a net mass loss from the accretor -> if in active RLO, gamma shold be zero
+	     !write(*,*) "MT_gamma: ",  xfer_frac_rlo, b% time_step, b% m_old(b% a_i) - b% m(b% a_i)
+         end if
          xfer_frac_rlo = min( max(0.0_dp, xfer_frac_rlo), 1.0_dp) ! bounded in [0,1]
 
          ! Calculate edot contribution - Eqn 19, Sepinsky et al (2009)
