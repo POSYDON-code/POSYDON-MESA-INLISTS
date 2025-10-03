2da34e6d9bb2dfd337d5cd8902f3b3cacd624e60
ZepeiX <77278187+ZepeiX@users.noreply.github.com>
2025-10-03 16:16:05 -0500

    Update run_binary_extras.f

--- r11701/default_common_inlists/binary/src/run_binary_extras.f
+++ r11701/default_common_inlists/binary/src/run_binary_extras.f
@@ -66,7 +66,46 @@
           b% other_tsync => my_tsync
           b% other_mdot_edd => my_mdot_edd
 	  b% other_rlo_mdot => my_rlo_mdot
+	      b% other_accreted_material_j => my_accreted_material_j
       end subroutine extras_binary_controls
+
+      subroutine my_accreted_material_j(binary_id, ierr)
+         use const_def, only: dp
+         integer, intent(in) :: binary_id
+         integer, intent(out) :: ierr
+         real(dp) :: qratio, min_r
+         type (binary_info), pointer :: b
+         ierr = 0
+         call binary_ptr(binary_id, b, ierr)
+         if (ierr /= 0) then
+            write(*,*) 'failed in binary_ptr'
+            return
+         end if
+         qratio = b% m(b% a_i) / b% m(b% d_i)
+         qratio = min(max(qratio,0.0667d0),15d0)
+         min_r = 0.0425d0*b% separation*pow_cr(qratio+qratio*qratio, 0.25d0)
+         if (b% r(b% a_i) < min_r) then
+            b% accretion_mode = 2
+            b% s_accretor% accreted_material_j = &
+                (1.5d0/(1d0+pow_cr(2.7183d0, &
+                10d0*(b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf-0.83d0)))-0.5d0) *&
+                sqrt(b% s_accretor% cgrav(1) * b% m(b% a_i) * b% r(b% a_i))
+         write(*,*) 'j1', b% r(b% a_i), min_r, b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf,&
+                     b% s_accretor% accreted_material_j
+         else
+            b% accretion_mode = 1
+            b% s_accretor% accreted_material_j = &
+                 (1.5d0/(1d0+pow_cr(2.7183d0, &
+                 10d0*(b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf-0.83d0)))-0.5d0)*&
+                sqrt(b% s_accretor% cgrav(1) * b% m(b% a_i) * 1.7d0*min_r)
+         write(*,*) 'j2', b% r(b% a_i), min_r, b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf,&
+                     b% s_accretor% accreted_material_j
+         end if
+         b% acc_am_div_kep_am = b% s_accretor% accreted_material_j / &
+             sqrt(b% s_accretor% cgrav(1) * b% m(b% a_i) * b% r(b% a_i))
+        
+      end subroutine my_accreted_material_j
+
 
       subroutine my_tsync(id, sync_type, Ftid, qratio, m, r_phot, osep, t_sync, ierr)
          integer, intent(in) :: id
@@ -1209,7 +1248,8 @@
          integer :: ierr, star_id, i
          real(dp) :: q, mdot_limit_low, mdot_limit_high, &
             center_h1, center_h1_old, center_he4, center_he4_old, &
-            rl23,rl2_1,trap_rad, mdot_edd
+            rl23,rl2_1,trap_rad, mdot_edd,Lrad_div_Ledd,gamma_factor,&
+			omega_crit, qratio, min_r
          logical :: is_ne_biggest
          real(dp) :: gamma1_integral, integral_norm, Pdm_over_rho
 
@@ -1518,6 +1558,29 @@
 	        end if
              end if
 	 end if
+
+	 qratio = b% m(b% a_i) / b% m(b% d_i)
+     qratio = min(max(qratio,0.0667d0),15d0)
+     min_r = 0.0425d0*b% separation*pow_cr(qratio+qratio*qratio,0.25d0)
+     if (b% r(b% a_i) < min_r) then
+         b% mass_transfer_beta =(1.5d0-1.5d0/(1.0d0+pow_cr(2.7183d0, &
+                               10d0*(b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf-0.83))))/&
+                               (sqrt((1.7d0*min_r)/b% r(b% a_i))-&
+                               (1.5d0/(1.0d0+pow_cr(2.7183d0, &
+                               10d0*(b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf-0.83)))-0.5d0))
+        write(*,*) 'm1', b% r(b% a_i), min_r, b% rl(b% a_i),b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf,&
+                     b% mass_transfer_beta
+    else
+        b% mass_transfer_beta = (1.5d0-1.5d0/(1.0d0+pow_cr(2.7183d0, &
+                               10d0*(b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf-0.83))))/&
+                               (sqrt(b% rl(b% a_i)/(1.7d0*min_r))-&
+                               (1.5d0/(1.0d0+pow_cr(2.7183d0, &
+                               10d0*(b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf-0.83)))-0.5d0))
+        write(*,*) 'm2', b% r(b% a_i), min_r, b% rl(b% a_i),b% s_accretor% omega_avg_surf/b% s_accretor% omega_crit_avg_surf,&
+                     b% mass_transfer_beta
+    end if
+    b% mass_transfer_beta = max(0d0,b% mass_transfer_beta)
+
 
       end function extras_binary_finish_step
 
