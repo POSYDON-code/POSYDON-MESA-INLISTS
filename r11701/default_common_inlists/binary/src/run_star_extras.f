1de0c0fa7cdebdf29f9b994c53d0e25e0b17939e
Abhishek <a.chattaraj@ufl.edu>
2026-01-24 15:51:31 -0600

    Reduced Nugis&Lamers mass loss rates by a factor of 100

--- r11701/default_common_inlists/binary/src/run_star_extras.f
+++ r11701/default_common_inlists/binary/src/run_star_extras.f
@@ -1713,7 +1713,7 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
       include 'formats'
       if (surface_h1 < 0.4d0) then ! helium rich Wolf-Rayet star: Nugis & Lamers
          ! w = 1d-11 * pow_cr(L1/Lsun,1.29d0) * pow_cr(Y,1.7d0) * sqrt(Zsurf) 
-         w = 1d-12 * pow_cr(L1/Lsun,1.29d0) * pow_cr(Y,1.7d0) * sqrt(Zsurf) 
+         w = 1d-13 * pow_cr(L1/Lsun,1.29d0) * pow_cr(Y,1.7d0) * sqrt(Zsurf) 
          if (dbg) write(*,1) 'Dutch_wind = Nugis & Lamers', log10_cr(wind)
          current_wind_prscr(id) = 2d0
       else
