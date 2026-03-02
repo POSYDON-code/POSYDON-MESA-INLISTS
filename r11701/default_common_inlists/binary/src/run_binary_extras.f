81453819b3a57063debef0f385ef613ada2cc97f
ZepeiX <77278187+ZepeiX@users.noreply.github.com>
2026-03-02 11:51:37 -0600

    Run for POSYDON
    Comment out termination conditions related to secondary reaching TAMS and reverse mass transfer.

--- r11701/default_common_inlists/binary/src/run_binary_extras.f
+++ r11701/default_common_inlists/binary/src/run_binary_extras.f
@@ -1334,17 +1334,17 @@
             end if
           end if
 
-         if (b% s1% center_h1 < 1.0d-6 .and. b% s2% center_h1 < 1.0d-6) then
-		     write(*,'(g0)') "termination code: Secondary reaches TAMS"
-             extras_binary_finish_step = terminate
-             return
-		 end if
-		 if (b% s1% center_h1 < 1.0d-6 .and. b% rl_relative_gap(2) > 0.0_dp &
-		     .and.  abs(b% mtransfer_rate/(Msun/secyer))>1.0d-10) then
-		     write(*,'(g0)') "termination code: reverse mass transfer"
-             extras_binary_finish_step = terminate
-             return
-		 end if
+         !if (b% s1% center_h1 < 1.0d-6 .and. b% s2% center_h1 < 1.0d-6) then
+		 !    write(*,'(g0)') "termination code: Secondary reaches TAMS"
+         !    extras_binary_finish_step = terminate
+         !    return
+		 !end if
+		 !if (b% s1% center_h1 < 1.0d-6 .and. b% rl_relative_gap(2) > 0.0_dp &
+		 !    .and.  abs(b% mtransfer_rate/(Msun/secyer))>1.0d-10) then
+		 !    write(*,'(g0)') "termination code: reverse mass transfer"
+         !    extras_binary_finish_step = terminate
+         !    return
+		 !end if
 		  
          ! check for termination due to carbon depletion
          if (b% point_mass_i /= 1) then
