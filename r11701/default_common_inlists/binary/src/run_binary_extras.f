dd87ef99dc7d460b52e5e492e0e736f7783a64ad
Seth Gossage <sethg45@gmail.com>
2026-06-14 13:11:27 -0500

    fix typo

--- r11701/default_common_inlists/binary/src/run_binary_extras.f
+++ r11701/default_common_inlists/binary/src/run_binary_extras.f
@@ -2295,7 +2295,7 @@
 
          ! check if accreting onto a cooling white dwarf
          if (b% point_mass_i == 0) then
-             if (b% s_accretor% central_gamma >= 3d0 .and. abs(b% mtransfer_rate/(Msun/secyer)) >= 1d-12) then
+             if (b% s_accretor% center_gamma >= 3d0 .and. abs(b% mtransfer_rate/(Msun/secyer)) >= 1d-12) then
                   write(*,'(g0)') "termination code: Mass transfer onto a cooling white dwarf"
                   extras_binary_finish_step = terminate
                   return
