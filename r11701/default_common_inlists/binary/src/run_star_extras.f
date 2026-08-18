cba762bffc6eb705e68900f79421c3d49418be61
ZepeiX <77278187+ZepeiX@users.noreply.github.com>
2026-08-18 05:08:14 -0500

    Adjust wind calculation by normalizing with 3

--- r11701/default_common_inlists/binary/src/run_star_extras.f
+++ r11701/default_common_inlists/binary/src/run_star_extras.f
@@ -1672,7 +1672,7 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
       end if
 
       w = alfa*w1 + (1d0 - alfa)*w2
-      w = w * 1d0/3d0
+      
       if (dbg) write(*,*) 'vink wind', w
 
     end subroutine eval_Vink_wind
@@ -1716,6 +1716,7 @@ subroutine loop_conv_layers(s,n_conv_regions_posydon, n_zones_of_region, bot_bdy
          current_wind_prscr(id) = 2d0
       else
          call eval_Vink_wind(w)
+         w = w * 1d0/3d0
          current_wind_prscr(id) = 1d0
       end if
 
